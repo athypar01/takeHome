44613987a6e3f79328b083a1754c1877c190a044
athypar <aby.thyparambil@tsworkz.com>
2021-10-13 11:49:53 -0500

    chore: fixed spec issues with frnds-app services

--- libs/user/src/lib/user/services/frnds_app.service.spec.ts
+++ libs/user/src/lib/user/services/frnds_app.service.spec.ts
@@ -1,6 +1,20 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule, Store } from '@ngrx/store';
 
+import { ConfirmationModule, ConfirmationService } from '@secureworks/confirmation';
+import { IconsShareableModule } from '@secureworks/icons';
+import { MockApiRequestsModule } from '@secureworks/mockApiRequests';
+import { FRNDS_APP_FEATURE_KEY, initReducer } from '../+state/reducers/frnds_app_init.reducer';
+import { ListComponent } from '../components/list/list.component';
 import { FrndsAppService } from './frnds_app.service';
 
 describe('FrndsAppService', () => {
@@ -9,7 +23,29 @@ describe('FrndsAppService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatSidenavModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MockApiRequestsModule.forRoot([]),
+        IconsShareableModule.forRoot(),
+        MatIconModule,
+        MatInputModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        ConfirmationModule,
+        StoreModule.forRoot({}),
+        StoreModule.forFeature(
+          FRNDS_APP_FEATURE_KEY,
+          initReducer
+        )
+      ],
+      declarations: [ ListComponent ],
+      providers: [
+        ConfirmationService,
+        Store,
       ]
     });
     service = TestBed.inject(FrndsAppService);
